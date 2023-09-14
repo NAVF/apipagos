13bc826dc7cc341686774f70a30952ef4985e24d
Neil Armstrong Vasquez Faustino <nvasquef@emeal.nttdata.com>
2023-09-14 13:04:06 -0500

    primer despliegue de servicios

---
+++ src/main/java/com/example/apipagos/ApipagosApplication.java
@@ -0,0 +1,13 @@
+package com.example.apipagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipagosApplication.class, args);
+	}
+
+}
